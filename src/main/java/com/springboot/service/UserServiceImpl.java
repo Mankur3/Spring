358b9222f8d041bfa4c842f4e04b4ac49b3e283e
Ankur <mathur.ankur.13@gmail.com>
2019-10-02 04:04:54 +0530

    changing data

--- src/main/java/com/springboot/service/UserServiceImpl.java
+++ src/main/java/com/springboot/service/UserServiceImpl.java
@@ -74,10 +74,10 @@ public class UserServiceImpl implements UserService{
 
 	private static List<User> populateDummyUsers(){
 		List<User> users = new ArrayList<User>();
-		users.add(new User(counter.incrementAndGet(),"Rishi",26, 70000));
-		users.add(new User(counter.incrementAndGet(),"Manish",24, 30000));
-		users.add(new User(counter.incrementAndGet(),"Kunal",27, 90000));
-		users.add(new User(counter.incrementAndGet(),"Pradeep",25, 40000));
+		users.add(new User(counter.incrementAndGet(),"ankur",26, 70000));
+		users.add(new User(counter.incrementAndGet(),"ankur",24, 30000));
+		users.add(new User(counter.incrementAndGet(),"ankur",27, 90000));
+		users.add(new User(counter.incrementAndGet(),"ankur",25, 40000));
 		return users;
 	}
 
