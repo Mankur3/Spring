5e8348e8acaa3c9b4a849f352736540e27fb17da
Ankur <mathur.ankur.13@gmail.com>
2019-10-02 04:16:36 +0530

    adding data mitra comit 4

--- src/main/java/com/springboot/service/UserServiceImpl.java
+++ src/main/java/com/springboot/service/UserServiceImpl.java
@@ -74,10 +74,10 @@ public class UserServiceImpl implements UserService{
 
 	private static List<User> populateDummyUsers(){
 		List<User> users = new ArrayList<User>();
-		users.add(new User(counter.incrementAndGet(),"aditya",26, 70000));
-		users.add(new User(counter.incrementAndGet(),"aditya",24, 30000));
-		users.add(new User(counter.incrementAndGet(),"aditya",27, 90000));
-		users.add(new User(counter.incrementAndGet(),"aditya",25, 40000));
+		users.add(new User(counter.incrementAndGet(),"mitra",26, 70000));
+		users.add(new User(counter.incrementAndGet(),"mitra",24, 30000));
+		users.add(new User(counter.incrementAndGet(),"mitra",27, 90000));
+		users.add(new User(counter.incrementAndGet(),"mitra",25, 40000));
 		return users;
 	}
 
