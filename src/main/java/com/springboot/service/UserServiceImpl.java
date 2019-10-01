0dc9562f73e3c4da732309f445f63ec4203dccd2
Ankur <mathur.ankur.13@gmail.com>
2019-10-02 04:56:52 +0530

    ankur

--- src/main/java/com/springboot/service/UserServiceImpl.java
+++ src/main/java/com/springboot/service/UserServiceImpl.java
@@ -74,10 +74,10 @@ public class UserServiceImpl implements UserService{
 
 	private static List<User> populateDummyUsers(){
 		List<User> users = new ArrayList<User>();
-		users.add(new User(counter.incrementAndGet(),"Tester",26, 70000));
-		users.add(new User(counter.incrementAndGet(),"Tester",24, 30000));
-		users.add(new User(counter.incrementAndGet(),"Tester",27, 90000));
-		users.add(new User(counter.incrementAndGet(),"Tester",25, 40000));
+		users.add(new User(counter.incrementAndGet(),"Ankur",26, 70000));
+		users.add(new User(counter.incrementAndGet(),"Ankur",24, 30000));
+		users.add(new User(counter.incrementAndGet(),"Ankur",27, 90000));
+		users.add(new User(counter.incrementAndGet(),"Ankur",25, 40000));
 		return users;
 	}
 
