9a45261c42feb1ee8fe7f7d59de66b8822925ad7
Ankur <mathur.ankur.13@gmail.com>
2019-10-02 01:54:48 +0530

    Refactor 2

--- src/main/java/com/springboot/SpringBootRestApiApp.java
+++ src/main/java/com/springboot/SpringBootRestApiApp.java
@@ -7,8 +7,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages={"com.springboot"})
 
-//Using @SpringBootApplication same as @Configuration @EnableAutoConfiguration @ComponentScan combined
-
 public class SpringBootRestApiApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
