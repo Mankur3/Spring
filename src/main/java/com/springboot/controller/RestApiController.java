3e261e4419092ab74ba0c3e45e0c34f23d777c88
Ankur <mathur.ankur.13@gmail.com>
2019-10-06 23:03:17 +0530

    changing response to json

--- src/main/java/com/springboot/controller/RestApiController.java
+++ src/main/java/com/springboot/controller/RestApiController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -47,7 +48,7 @@ public class RestApiController {
 
 	// --------Single User retrieval----------
 
-	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET,  produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> getUser(@PathVariable("id") long id) {
 		logger.info("Fetching User with id {}", id);
 		User user = userService.findById(id);
